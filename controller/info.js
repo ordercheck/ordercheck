6fa2ce194fbdf90151d5082acaab5694c7dfa772
KimGiTae <rlxo6919@naver.com>
2022-01-19 15:41:11 +0900

    [2022.01.19] 수정

--- controller/info.js
+++ controller/info.js
@@ -2,7 +2,20 @@ const db = require('../model/db');
 
 module.exports = {
   getUserProfile: async (req, res) => {
-    const result = await db.user.findByPk(req.loginUser);
+    const result = await db.user.findByPk(req.loginUser, {
+      include: [
+        {
+          model: db.userCompany,
+          include: [
+            {
+              model: db.company,
+              include: [{ model: db.plan, attributes: ['planIdx'] }],
+            },
+          ],
+        },
+      ],
+      attributes: { exclude: ['user_password'] },
+    });
     return res.send({ success: 200, result });
   },
 };
