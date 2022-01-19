d76ece3890519783604ae0c5fa55c2f09d340a47
KimGiTae <rlxo6919@naver.com>
2022-01-19 13:54:15 +0900

    [2022.01.19] filter 정보 보여주기 + 페이징

--- controller/consultingFilter.js
+++ controller/consultingFilter.js
@@ -1,4 +1,5 @@
 const { checkUserCompany } = require('../lib/apiFunctions');
+const { changeDate } = require('../lib/apiFunctions');
 const db = require('../model/db');
 const { Op } = require('sequelize');
 // 받은 params를 sequelize or형태에 맞게 만들어주는 함수
@@ -21,13 +22,7 @@ module.exports = {
       if (checkResult == false) {
         return res.send({ success: 400 });
       }
-
-      date = date.replace(/ /gi, '').split('-');
-
-      const firstDate = new Date(date[0].replace(/\./gi, '-'));
-      const secondDate = new Date(date[1].replace(/\./gi, '-'));
-      firstDate.setDate(firstDate.getDate() - 1);
-      secondDate.setDate(secondDate.getDate() + 1);
+      const { firstDate, secondDate } = changeDate(date);
 
       const result = await db.consulting.count({
         where: {
--- controller/consultingShow.js
+++ controller/consultingShow.js
@@ -1,4 +1,5 @@
 const { checkUserCompany } = require('../lib/apiFunctions');
+const { changeDate } = require('../lib/apiFunctions');
 const db = require('../model/db');
 const { Op } = require('sequelize');
 module.exports = {
@@ -152,43 +153,74 @@ module.exports = {
     }
   },
   showFilterResult: async (req, res) => {
-    const { company_idx } = req.query;
+    let { company_idx, date, limit, page } = req.query;
+    // 주어진 조건으로 데이터를 찾기
+    const findData = async (
+      type,
+      offset,
+      limit,
+      activeArrResult,
+      possibilityArrResult,
+      firstDate,
+      secondDate
+    ) => {
+      try {
+        const result = await db.customer[type]({
+          where: {
+            company_idx,
+            createdAt: { [Op.between]: [firstDate, secondDate] },
+            active: { [Op.in]: activeArrResult },
+            contract_possibility: { [Op.in]: possibilityArrResult },
+          },
+          offset,
+          limit,
+          order: [['createdAt', 'DESC']],
+        });
+        return result;
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    // 페이징을 위한 int처리
+    limit = parseInt(limit);
+    // 가져올 시작페이지 설정
+    const start = (page - 1) * limit;
     try {
-      const makeArrforFilter = (data, status) => {
-        try {
-          let countArr = data.split(',');
-          countArr = countArr.map((el) => {
-            el = { [status]: el };
-            return el;
-          });
-          return countArr;
-        } catch (err) {
-          return;
-        }
-      };
-      const activeArrResult = makeArrforFilter(
-        req.query.active,
-        (status = 'active')
+      // 날짜변환
+      const { firstDate, secondDate } = changeDate(date);
+      // 주어진 데이터를 Arr형태로 변경
+      const activeArrResult = req.query.active
+        ? req.query.active.split(',')
+        : ['상담 신청', '상담완료', '이슈'];
+      const possibilityArrResult = req.query.contract_possibility
+        ? req.query.contract_possibility.split(',')
+        : ['없음', '50%이상', '50%미만'];
+      // 총 페이지 수 체크
+      const totalPage = await findData(
+        'count',
+        null,
+        null,
+        activeArrResult,
+        possibilityArrResult,
+        firstDate,
+        secondDate
       );
 
-      const possibilityArrResult = makeArrforFilter(
-        req.query.contract_possibility,
-        (status = 'contract_possibility')
+      // 요구 만큼 데이터 가져오기
+      const result = await findData(
+        'findAll',
+        start,
+        limit,
+        activeArrResult,
+        possibilityArrResult,
+        firstDate,
+        secondDate
       );
-      console.log(activeArrResult);
-      console.log(possibilityArrResult);
-      const totalArr = activeArrResult.concat(possibilityArrResult);
 
-      const result = await db.customer.findAll({
-        where: {
-          company_idx,
-          [Op.or]: totalArr,
-        },
-        order: [['createdAt', 'DESC']],
-      });
-      return res.send({ result });
+      return res.send({ result, totalPage: Math.ceil(totalPage / limit) });
     } catch (err) {
-      console.log('bye');
+      const Err = err.message;
+      return res.send({ success: 500, Err });
     }
   },
 };
