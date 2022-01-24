1c02c8ce2d23c99535101309041e01c0dd2a13df
KimGiTae <rlxo6919@naver.com>
2022-01-24 13:38:29 +0900

    수정2

--- router/api/schedulePay.js
+++ router/api/schedulePay.js
@@ -3,9 +3,9 @@ const router = express.Router();
 // 정기 결제 완료 후 다음달 결제 예약
 router.post('/', (req, res) => {
   const { imp_uid, merchant_uid, status } = req.body;
-  console.log(imp_uid);
-  console.log(merchant_uid);
-  console.log(status);
+  console.log('imp_uid', imp_uid);
+  console.log(' merchant_uid', merchant_uid);
+  console.log(' status', status);
   if (status == 'paid') {
     const changeToTime = new Date();
     const changeToUnix = changeToTime.getTime() / 1000;
