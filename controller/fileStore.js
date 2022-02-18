9efc2735ac0d47a5ded26c294c787425478b8df4
KimGiTae <rlxo6919@naver.com>
2022-02-18 15:54:14 +0900

    수정

--- controller/fileStore.js
+++ controller/fileStore.js
@@ -323,6 +323,7 @@ module.exports = {
         urlArr[urlArr.length - 1] = titleAndExtend.join('.');
         const file_url = urlArr.join('/');
 
+        console.log(findFilesResult.title);
         //  params만들기
         params = checkFile(
           req,
