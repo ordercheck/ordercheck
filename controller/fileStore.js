45a61a9683f6e77d1c79ba8f1e3d7fe927a47d69
KimGiTae <rlxo6919@naver.com>
2022-02-21 17:48:39 +0900

    파일변경 테스트

--- controller/fileStore.js
+++ controller/fileStore.js
@@ -302,6 +302,7 @@ module.exports = {
   },
   changeFileTitle: async (req, res, next) => {
     console.log('요청 들어옴');
+    console.log(req);
     try {
       const {
         body: { uuid, title, isFolder },
