10b4bdb665c363347fe3b6ec557d528653843443
KimGiTae <rlxo6919@naver.com>
2022-02-22 09:49:20 +0900

    제목 변경 후 response data 수정

--- controller/formLink.js
+++ controller/formLink.js
@@ -227,7 +227,7 @@ module.exports = {
       let formDetail = await db.formLink.findByPk(formId, {
         attributes: createFormLinkAttributes,
       });
-      formList = formList.map((data) => {
+      formDetail = formDetail.map((data) => {
         data.urlPath = `${data.form_link}/${data.expression}`;
         return data;
       });
