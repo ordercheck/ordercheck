f51a72e56abec7ea93de80bf68abbbaec15907fd
KimGiTae <rlxo6919@naver.com>
2022-02-28 18:22:19 +0900

    cors 수정

--- controller/socket.js
+++ controller/socket.js
@@ -2,7 +2,7 @@ const db = require('../model/db');
 module.exports = {
   socketTest: async (req, res, next) => {
     const io = req.app.get('io');
-
-    io.to('1').emit('sendAlarm', '알람 보냅니다');
+    io.emit('sendAlarm', '알람 보냅니다');
+    // io.to('1').emit('sendAlarm', '알람 보냅니다');
   },
 };
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const db = require('./model/db');
 const { joinFunction } = require('./lib/apiFunctions');
 io.on('connection', (socket) => {
   console.log('연결됨');
-  socket.on('alarm', async (data) => {
-    socket.join(`${data}`);
-  });
+  // socket.on('alarm', async (data) => {
+  //   socket.join(`${data}`);
+  // });
 });
