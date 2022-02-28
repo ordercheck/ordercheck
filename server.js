5929017e0eab054b37a4c8309a89566c7d4cf76f
KimGiTae <rlxo6919@naver.com>
2022-02-28 18:18:46 +0900

    cors 수정

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const io = require('./setting');
 const db = require('./model/db');
 const { joinFunction } = require('./lib/apiFunctions');
 io.on('connection', (socket) => {
+  console.log('연결됨');
   socket.on('alarm', async (data) => {
     socket.join(`${data}`);
   });
