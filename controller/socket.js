af8e9adaec9ba827df11a85937e591110fdc409f
KimGiTae <rlxo6919@naver.com>
2022-02-28 18:24:04 +0900

    cors 수정

--- controller/socket.js
+++ controller/socket.js
@@ -2,6 +2,7 @@ const db = require('../model/db');
 module.exports = {
   socketTest: async (req, res, next) => {
     const io = req.app.get('io');
+    console.log('알람 보내기');
     io.emit('sendAlarm', '알람 보냅니다');
     // io.to('1').emit('sendAlarm', '알람 보냅니다');
   },
