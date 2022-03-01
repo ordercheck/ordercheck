b6b3ece3ed708ac08abe5257462fcda2aa7733aa
KimGiTae <rlxo6919@naver.com>
2022-03-01 14:00:21 +0900

    영수증 db 추가

---
+++ model/receipt.js
@@ -0,0 +1,67 @@
+module.exports = function (sequelize, DataTypes) {
+  //테이블을 정의
+  return sequelize.define('receipt', {
+    idx: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+
+    company_name: {
+      allowNull: false,
+
+      type: DataTypes.STRING(100),
+    },
+
+    receiptId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+
+    receipt_kind: {
+      allowNull: false,
+
+      type: DataTypes.STRING(100),
+    },
+    card_number: {
+      allowNull: true,
+      type: DataTypes.STRING(100),
+    },
+
+    status: {
+      allowNull: true,
+      defaultValue: true,
+      type: DataTypes.BOOLEAN,
+    },
+    result_price: {
+      allowNull: true,
+      type: DataTypes.STRING(100),
+    },
+
+    result_price_levy: {
+      allowNull: true,
+      type: DataTypes.STRING(100),
+    },
+
+    whiteLabel_price: {
+      allowNull: true,
+      type: DataTypes.STRING(100),
+    },
+
+    chat_price: {
+      allowNull: true,
+
+      type: DataTypes.STRING(100),
+    },
+    analystic_price: {
+      allowNull: true,
+
+      type: DataTypes.STRING(100),
+    },
+
+    merchant_uid: {
+      allowNull: true,
+      type: DataTypes.INTEGER,
+    },
+  });
+};
