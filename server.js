e6539d32bb85acc9afbed27d5fee2057e2fbdf9d
KimGiTae <rlxo6919@naver.com>
2022-03-02 10:56:19 +0900

    소켓 room 만들기

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 'use strict';
 require('dotenv').config();
-
 const io = require('./setting');
 const db = require('./model/db');
+const verify_data = require('./lib/jwtfunctions');
 const { joinFunction } = require('./lib/apiFunctions');
 io.on('connection', (socket) => {
   console.log('연결됨');
-  // socket.on('alarm', async (data) => {
-  //   socket.join(`${data}`);
-  // });
+  socket.on('alarm', async (data) => {
+    const user = verify_data(data);
+    console.log(user);
+    socket.join(`${data}`);
+  });
 });
