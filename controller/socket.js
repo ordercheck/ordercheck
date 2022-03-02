a84d51d898c4f78af5f8472c736082617e7aa72d
KimGiTae <rlxo6919@naver.com>
2022-03-02 13:38:56 +0900

    join room 변경

--- controller/socket.js
+++ controller/socket.js
@@ -3,7 +3,7 @@ module.exports = {
   socketTest: async (req, res, next) => {
     const io = req.app.get('io');
     console.log('알람 보내기');
-    io.emit('sendAlarm', '알람 보냅니다');
-    // io.to('1').emit('sendAlarm', '알람 보냅니다');
+    console.log(io.sockets);
+    io.to('1').emit('sendAlarm', '알람 보냅니다');
   },
 };
--- server.js
+++ server.js
@@ -6,23 +6,33 @@ const verify_data = require('./lib/jwtfunctions');
 const { joinFunction } = require('./lib/apiFunctions');
 io.on('connection', (socket) => {
   console.log('연결됨');
-  socket.on('alarm', async (data) => {
+  // 연결 하자마자 알림
+
+  socket.on('alarmJoin', async (data) => {
     // 토큰으로 user idx 찾기
     const user = await verify_data(data);
-
     // 소속 회사 idx 찾기
     const findUserCompanyResult = await db.userCompany.findOne({
       where: { user_idx: user.user_idx, deleted: null, active: 1 },
       attributes: ['company_idx'],
     });
+    // 회사 room 참가
+    socket.join(findUserCompanyResult.company_idx);
+  });
 
+  socket.on('alarm', async (data) => {
+    // 토큰으로 user idx 찾기
+    const user = await verify_data(data);
+    // 소속 회사 idx 찾기
+    const findUserCompanyResult = await db.userCompany.findOne({
+      where: { user_idx: user.user_idx, deleted: null, active: 1 },
+      attributes: ['company_idx'],
+    });
     // 회사 alarm 찾기
     const findResult = await db.alarm.findAll({
       where: { company_idx: findUserCompanyResult.company_idx },
       attributes: ['idx', 'message', 'createdAt'],
     });
-    socket.join('1');
-    console.log(io.sockets.adapter.rooms);
-    socket.in('1').emit('sendAlarm', '보내져라 룸!');
+    io.to(findUserCompanyResult.company_idx).emit('sendAlarm', findResult);
   });
 });
