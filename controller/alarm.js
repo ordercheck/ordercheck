a76fcfaf919c015ec6904589f86b3f7748c6e9cf
KimGiTae <rlxo6919@naver.com>
2022-03-02 18:18:18 +0900

    alarm test

--- controller/alarm.js
+++ controller/alarm.js
@@ -5,19 +5,32 @@ module.exports = {
   delAlarm: async (req, res, next) => {
     const {
       params: { alarmId },
+      user_idx,
     } = req;
+
     await db.alarm.destroy({ where: { idx: alarmId } });
-    return res.send({ success: 200, message: '삭제 완료' });
+
+    const io = req.app.get('io');
+
+    const findResult = await db.alarm.findAll({
+      where: { user_idx, repeat_time: null },
+      raw: true,
+    });
+    io.to(user_idx).emit('sendAlarm', findResult);
+    return;
   },
 
   confirmAlarm: async (req, res, next) => {
     const { alarmId } = req.body;
-
     alarmId.forEach(async (data) => {
       await db.alarm.update({ confirm: true }, { where: { idx: data } });
     });
 
-    return res.send({ success: 200, message: '확인 완료' });
+    const findResult = await db.alarm.findAll({
+      where: { user_idx, repeat_time: null },
+      raw: true,
+    });
+    io.to(user_idx).emit('sendAlarm', findResult);
   },
   repeatAlarm: async (req, res, next) => {
     const {
