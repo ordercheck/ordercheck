6cd93f3661ae3ce462f3dfde84dd36354eec2b29
KimGiTae <rlxo6919@naver.com>
2022-03-02 13:08:47 +0900

    join room 변경

--- server.js
+++ server.js
@@ -23,8 +23,6 @@ io.on('connection', (socket) => {
     });
     socket.join(`${findUserCompanyResult.company_idx}`);
 
-    socket
-      .to(`${findUserCompanyResult.company_idx}`)
-      .emit('sendAlarm', '보내져라 메세지');
+    socket.emit('sendAlarm', '보내져라 메세지');
   });
 });
