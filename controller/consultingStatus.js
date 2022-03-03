8b2e3e7aac2072390e07695b0d209427862a9fc6
KimGiTae <rlxo6919@naver.com>
2022-03-03 16:16:49 +0900

    폼 신청 테스트

--- controller/consultingStatus.js
+++ controller/consultingStatus.js
@@ -148,7 +148,7 @@ module.exports = {
       if (bodyClass.bodyData.tempType == 1) {
         bodyClass.bodyData.choice = bodyClass.bodyData.choice.join(', ');
         console.log('여기 타라');
-        createConsultingAndIncrement(bodyClass.bodyData.data);
+        createConsultingAndIncrement(bodyClass.bodyData);
         return;
       }
 
