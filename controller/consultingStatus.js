b5e3ed37784d0944e3f3d814883321fae7a75309
KimGiTae <rlxo6919@naver.com>
2022-03-04 10:17:43 +0900

    config 수정

--- controller/consultingStatus.js
+++ controller/consultingStatus.js
@@ -309,6 +309,7 @@ module.exports = {
       const message = `${findUser.user_name}님이 [${findCustomer.customer_name} ${now}]을 신규 등록했습니다.`;
 
       const io = req.app.get('io');
+      console.log('hi');
       const findMembers = await findMemberExceptMe(company_idx, user_idx);
       const expiry_date = createExpireDate();
       console.log(expiry_date);
