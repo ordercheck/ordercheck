a5d47d9eca066ff21454bc8c85b973bc21b85e68
KimGiTae <rlxo6919@naver.com>
2022-03-04 17:57:38 +0900

    폼링크 생성 수정

--- controller/home.js
+++ controller/home.js
@@ -7,13 +7,29 @@ const {} = require('../lib/attributes');
 
 module.exports = {
   getHomeBoard: async (req, res, next) => {
-    const { company_idx } = req;
+    const { company_idx, user_idx } = req;
     const customerCount = await db.customer.count({
       where: { company_idx, deleted: null },
     });
 
-    // const customerCount = await db.customer.count({
-    //   where: { company_idx, deleted: null },
-    // });
+    const issueCustomerCount = await db.customer.count({
+      where: { company_idx, deleted: null, status: 4 },
+    });
+
+    const monthCount = await db.company.findByPk(company_idx, {
+      attributes: ['form_link_count'],
+    });
+
+    const unconfirmAlarm = await db.alarm.count({
+      where: { company_idx, confirm: false },
+    });
+
+    const userInfo = await db.user.findByPk(user_idx, {
+      attributes: ['user_profile', 'user_name'],
+    });
+
+    const standByMember = await db.userCompany.count({
+      where: { active: 0, deleted: null, company_idx },
+    });
   },
 };
