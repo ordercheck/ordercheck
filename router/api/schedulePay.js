48ee8a5274975e6ac674111974b80a06c9af5b1f
KimGiTae <rlxo6919@naver.com>
2022-03-10 12:46:43 +0900

    정기결제test

--- router/api/schedulePay.js
+++ router/api/schedulePay.js
@@ -78,7 +78,7 @@ router.post('/', async (req, res, next) => {
 
         const receiptId = generateRandomCode(6);
         delete findActivePlanResult.idx;
-
+        console.log('1', findActivePlanResult);
         await db.receipt.create({
           ...findActivePlanResult,
           card_name: getResult.card_name,
@@ -115,7 +115,7 @@ router.post('/', async (req, res, next) => {
           moment(startDate).format('YYYY.MM.DD');
 
         findActivePlanResult.expire_plan = nextExpireDate;
-        console.log(findActivePlanResult);
+        console.log('2', findActivePlanResult);
         // 새로운 결제 예약
         await db.plan.create({
           ...findActivePlanResult,
