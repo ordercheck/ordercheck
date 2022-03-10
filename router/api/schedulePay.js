64affad22a721a0cf95fb315ae1d19bea6c67baf
KimGiTae <rlxo6919@naver.com>
2022-03-10 11:54:20 +0900

    정기결제test

--- router/api/schedulePay.js
+++ router/api/schedulePay.js
@@ -70,7 +70,7 @@ router.post('/', async (req, res, next) => {
             },
           ],
         });
-        console.log(findCompanyName);
+        console.log(findCompanyName.company);
         const findCardNumber = await db.card.findOne({
           where: { customer_uid: getResult.customer_uid },
           attributes: ['card_number'],
