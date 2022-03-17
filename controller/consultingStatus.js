bc20e2b8f9edcf8643d5589154861ca58d7d6524
TAE6919 <rlxo6919@naver.com>
2022-03-17 22:20:53 +0900

    [2022.03.17 소켓오류]

--- controller/consultingStatus.js
+++ controller/consultingStatus.js
@@ -333,6 +333,7 @@ module.exports = {
         customer_idx: consultResult.idx,
       });
       const sendAlarm = new Alarm(createResult);
+      console.log(sendAlarm);
       io.to(parseInt(createResult.user_idx)).emit("addAlarm", sendAlarm);
       return;
     } catch (err) {
