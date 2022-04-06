39f72020c3147950c12145a80bdf32c8ef413c34
TAE6919 <rlxo6919@naver.com>
2022-04-06 19:13:15 +0900

    [2022.04.06] alimTalk 변경

--- controller/consultingStatus.js
+++ controller/consultingStatus.js
@@ -346,9 +346,8 @@ module.exports = {
       }
 
       res.send({ success: 200, consultResult });
-      console.log(typeof user_idx);
-      console.log(typeof contract_person);
-      if (user_idx !== contract_person) {
+
+      if (user_idx !== parseInt(contract_person)) {
         // 팀원에게 알림 보내기
         const check = await db.user.findByPk(user_idx, {
           attributes: ["user_name"],
