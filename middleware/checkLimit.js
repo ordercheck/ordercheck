beff8eb03b7e0751c263674029bf164d97b1e0a7
TAE6919 <rlxo6919@naver.com>
2022-04-07 10:42:04 +0900

    [2022.04.07] file보관함 100 알림톡

--- middleware/checkLimit.js
+++ middleware/checkLimit.js
@@ -262,7 +262,7 @@ module.exports = {
       );
     }
     // 100% 찼을 때
-    if (limitPlan[findPlanResult.plan].fileStore <= tbFileSize) {
+    if (true) {
       const alarmMessage = alarm.fileLimitAlarm100();
       const insertData = {
         message: alarmMessage,
