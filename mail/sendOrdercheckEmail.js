b80f183cb3f86624f254a83b7b92b617338d5aff
TAE6919 <rlxo6919@naver.com>
2022-04-08 14:44:43 +0900

    [2022.04.08] login check

--- mail/sendOrdercheckEmail.js
+++ mail/sendOrdercheckEmail.js
@@ -12,7 +12,7 @@ const transport = nodemailer.createTransport({
 
 const sendInviteEmail = async (company_url, company_name, inviter, target) => {
   let mailOptions = {
-    from: process.env.SEND_EMAIL_ID,
+    from: "rlxo6919@gmail.com",
     to: target,
     subject: "오더체크",
     html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
