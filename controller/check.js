137ce05d4606868cc73bcb470a09326630880199
TAE6919 <rlxo6919@naver.com>
2022-04-10 08:47:46 +0900

    회사 가입 수정

--- controller/check.js
+++ controller/check.js
@@ -8,32 +8,33 @@ module.exports = {
     } = req;
 
     // 멤버로 들어가있는지,
-    const checkCompany = await db.company.findOne({
+    const checkCompanyMember = await db.company.count({
       where: { company_subdomain },
+      include: [
+        {
+          model: db.userCompany,
+          where: {
+            user_idx,
+            active: true,
+            standBy: false,
+          },
+        },
+      ],
       attributes: ["idx"],
-    });
-
-    const checkCompanyMember = await db.userCompany.count({
-      where: {
-        company_idx: checkCompany.idx,
-        user_idx,
-        active: true,
-        standBy: false,
-      },
     });
 
     // 서브도메인 회사에 가입 안되어 있음
     if (checkCompanyMember == 0) {
       const checkCompany = await db.userCompany.findOne({
-        where: { user_idx },
+        where: { user_idx, active: true, standBy: false },
         attributes: ["company_idx"],
       });
 
-      const checkAnotherCompany = await db.company.findOne({
+      const checkAnotherCompany = await db.company.count({
         where: { idx: checkCompany.company_idx, companyexist: true },
       });
 
-      if (!checkAnotherCompany) {
+      if (checkAnotherCompany == 0) {
         console.log("다른회사 가입 안되 있음");
         //   다른회사 가입 안되있을 때
         return res.send({ success: 200, message: 0 });
