4797b00c60ef9d621663a90138004dd6704fcab2
TAE6919 <rlxo6919@naver.com>
2022-04-12 13:10:32 +0900

    [2022.04.12] 카드 등록 결제

--- controller/card.js
+++ controller/card.js
@@ -2,7 +2,9 @@ const db = require("../model/db");
 
 const { verify_data } = require("../lib/jwtfunctions");
 const { schedulePay } = require("../lib/payFunction");
-
+const moment = require("moment");
+require("moment-timezone");
+moment.tz.setDefault("Asia/Seoul");
 module.exports = {
   enrollmentCard: async (req, res, next) => {
     const {
