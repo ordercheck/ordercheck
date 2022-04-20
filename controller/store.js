edb8ee72653e9723832ef368c8cd82e678ece124
TAE6919 <rlxo6919@naver.com>
2022-04-20 18:32:25 +0900

    [2022.04.20] 플랜 변경 테스트

--- controller/store.js
+++ controller/store.js
@@ -7,20 +7,20 @@ module.exports = {
     try {
       const { body, user_idx } = req;
       // 제일 최근 데이터가 같은 데이터인지 체크
-      await client.connect();
+      // await client.connect();
 
-      const stroedBread = await client.get(`${user_idx}`);
+      // const stroedBread = await client.get(`${user_idx}`);
 
-      if (stroedBread == body.bread) {
-        console.log(stroedBread);
-      } else {
-        console.log(stroedBread);
-        body.user_idx = user_idx;
-        await db.store.create(body);
-        client.del(`${user_idx}`);
-        client.set(`${user_idx}`, `${body.bread}`);
-      }
-      await client.quit();
+      // if (stroedBread == body.bread) {
+      //   console.log(stroedBread);
+      // } else {
+      //   console.log(stroedBread);
+      //   body.user_idx = user_idx;
+      //   await db.store.create(body);
+      //   client.del(`${user_idx}`);
+      //   client.set(`${user_idx}`, `${body.bread}`);
+      // }
+      // await client.quit();
       return res.send({ success: 200 });
     } catch (err) {
       next(err);
