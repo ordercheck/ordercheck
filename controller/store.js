04f766ac115b2767165ff23bcc1aa85c68d1c291
TAE6919 <rlxo6919@naver.com>
2022-04-21 17:32:37 +0900

    [2022.04.21] ordercheckIo 구독 내역

--- controller/store.js
+++ controller/store.js
@@ -1,44 +1,33 @@
 const db = require("../model/db");
-// const redis = require("redis");
+const redis = require("redis");
 
-// const client = redis.createClient({
-//   host: "ordercheckcache.xnuldq.ng.0001.apn2.cache.amazonaws.com",
-//   port: 6379,
-// });
+const client = redis.createClient();
 module.exports = {
   storeBread: async (req, res, next) => {
     try {
       const { body, user_idx } = req;
 
       // io 인지 체크
-      // if (process.env.NODE_MODE == "IO") {
-      // 제일 최근 데이터가 같은 데이터인지 체크
-      // await client.connect();
-      // client.set("name", "zerocho");
-      // client.get("name", (err, reply) => {
-      //   console.log(reply);
-      // });
+      if (process.env.NODE_MODE == "IO") {
+        // 제일 최근 데이터가 같은 데이터인지 체크
+        await client.connect();
 
-      // client.get(`${user_idx}`, async (err, data) => {
-      //   if (err) {
-      //     console.log(err);
-      //   }
+        const storedBread = await client.get(`${user_idx}`);
 
-      //   if (data == body.bread) {
-      //     console.log(data);
-      //   } else {
-      //     console.log(data);
-      //     body.user_idx = user_idx;
-      //     await db.store.create(body);
-      //     client.del(`${user_idx}`);
-      //     client.set(`${user_idx}`, `${body.bread}`);
-      //   }
-      // });
-      // await client.exit();
-      return res.send({ success: 200 });
-      // } else {
-      //   return res.send({ success: 200 });
-      // }
+        if (storedBread == body.bread) {
+          console.log(storedBread);
+        } else {
+          console.log(storedBread);
+          body.user_idx = user_idx;
+          await db.store.create(body);
+          client.del(`${user_idx}`);
+          client.set(`${user_idx}`, `${body.bread}`);
+        }
+        await client.exit();
+        return res.send({ success: 200 });
+      } else {
+        return res.send({ success: 200 });
+      }
     } catch (err) {
       next(err);
     }
