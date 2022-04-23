cfb0e5138f47a32406687b9b99215c4ac1f1aa04
TAE6919 <rlxo6919@naver.com>
2022-04-23 12:12:55 +0900

    [2022.04.22] Io test

--- app.js
+++ app.js
@@ -173,7 +173,7 @@ class AppServer extends http.Server {
   }
 
   router() {
-    this.app.use(morgan("short"));
+    this.app.use(morgan("combined"));
     this.app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
     this.app.use("/", ordercheckRouter);
     this.app.use("/api", apiRouter);
