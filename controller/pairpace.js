f19d9af44554285fca533f1bfe02f6d59b5b0179
TAE6919 <rlxo6919@naver.com>
2022-05-04 16:44:42 +0900

    [2022.05.04] pairpace response 변경

--- controller/pairpace.js
+++ controller/pairpace.js
@@ -1,5 +1,6 @@
 const db = require("../model/db");
 const moment = require("moment");
+const jwt = require("jsonwebtoken");
 require("moment-timezone");
 moment.tz.setDefault("Asia/Seoul");
 module.exports = {
@@ -16,32 +17,36 @@ module.exports = {
       orderCheckClientContact: customer_phone,
       applyDate: submission_date,
       formType: form_type,
+      token,
       strPpAppliIdx,
     } = req.body;
-    console.log(submission_date);
     try {
-      await db.pairPace.create({
-        sender_idx,
-        customer_idx,
-        strPpAppliIdx,
-        post_address,
-        address,
-        jibun_address,
-        detail_address,
-        company_name,
-        customer_name,
-        customer_phone,
-        form_type,
-        submission_date,
-      });
-      console.log(req.body);
-      const io = req.app.get("io");
-      console.log(sender_idx);
-      io.to(+sender_idx).emit("closePairpace", {
-        isclosed: true,
-        sender_idx,
-      });
-      return res.send({ success: 200 });
+      const check = jwt.verify(token, process.env.PAIRPACE_JWT_SECRET);
+      console.log(check);
+      if (check) {
+        await db.pairPace.create({
+          sender_idx,
+          customer_idx,
+          strPpAppliIdx,
+          post_address,
+          address,
+          jibun_address,
+          detail_address,
+          company_name,
+          customer_name,
+          customer_phone,
+          form_type,
+          submission_date,
+        });
+
+        const io = req.app.get("io");
+
+        io.to(+sender_idx).emit("closePairpace", {
+          isclosed: true,
+          sender_idx,
+        });
+        return res.send({ success: 200 });
+      }
     } catch (err) {
       next(err);
     }
